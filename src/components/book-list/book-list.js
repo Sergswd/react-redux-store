a4872d1774fc6966db1f83dd9340de12ad37d2dc
Sergey Kh <falko.serega2016@yandex.ru>
2021-05-21 14:38:07 +0300

    create app and add styles

---
+++ src/components/book-list/book-list.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import BookListItem from '../book-list-item';
+import { connect } from 'react-redux';
+import { compose } from '../../utils';
+
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import './book-list.css';
+
+class BookList extends Component {
+
+  componentDidMount() {
+    // 1. receive data
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+    console.log(data);
+
+    // 2. dispatch action to store
+    this.props.booksLoaded(data);
+  }
+
+  render() {
+    const { books } = this.props;
+
+    return (
+      <ul className="book-list">
+        {
+          books.map((book) => {
+            return (
+              <li key={book.id}><BookListItem book={book} /></li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    books: state.books
+  };
+};
+
+const mapDispatchToProps = {
+  booksLoaded
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
